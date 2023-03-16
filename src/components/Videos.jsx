bbf20cb46cb3a743258e98d52b2edb1e207ca888
jrobles0717 <j_robles0717@yahoo.com>
2023-03-15 23:26:43 -0400

    working to fix youtube videos error

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -5,6 +5,34 @@ function Videos() {
     <section className={classes.section}>
       <div className={classes.container}>
         <h2 className={classes.title}>Videos</h2>
+        <div className={classes.containerVideo}>
+          <iframe
+            src="https://youtu.be/UmJEMCqjl3w"
+            className={classes.video}
+            width="500vh"
+            height="315"
+            title="Saudades"
+          ></iframe>
+          <iframe
+            src="https://youtu.be/d2feZqdh2Zg"
+            className={classes.video}
+            width="500vh"
+            height="315"
+            title="Antinomias"
+          ></iframe>
+          <iframe
+            src="https://www.youtube.com/watch?v=UmJEMCqjl3w"
+            className={classes.video}
+            width="500vh"
+            height="315"
+          ></iframe>
+          <iframe
+            src="https://youtu.be/UmJEMCqjl3w"
+            className={classes.video}
+            width="500vh"
+            height="315"
+          ></iframe>
+        </div>
       </div>
     </section>
   );
