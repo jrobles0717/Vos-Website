46f584a2a5deec2ae0f6332ef8c2499724f37e2a
jrobles0717 <j_robles0717@yahoo.com>
2023-03-15 21:56:37 -0400

    Icons added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,11 @@
+import {
+  BsApple,
+  BsFacebook,
+  BsInstagram,
+  BsSpotify,
+  BsTwitter,
+} from "react-icons/bs";
+
 import classes from "./Hero.module.css";
 
 function Hero() {
@@ -7,7 +15,7 @@ function Hero() {
         <div className={classes.imgBox}>
           <picture>
             <img
-              src="../../img/vos-img.jpg"
+              src="../../img/vos-img.png"
               className={classes.img}
               alt="Vos photo"
             />
@@ -15,13 +23,30 @@ function Hero() {
         </div>
         <div className={classes.textBox}>
           <h1 className={classes.headingPrimary}>VOS</h1>
-          <p className={classes.textOut}>out now</p>
+          <p className={classes.textOut}>listen now</p>
           <div className={classes.containerBtn}>
             <a href="#" className={classes.btn}>
               stream songs
             </a>
             <a href="#" className={classes.btn}>
               buy songs
+            </a>
+          </div>
+          <div className={classes.icons}>
+            <a href="#" className={classes.icon}>
+              <BsFacebook size={18} />
+            </a>
+            <a href="#" className={classes.icon}>
+              <BsTwitter size={18} />
+            </a>
+            <a href="#" className={classes.icon}>
+              <BsInstagram size={18} />
+            </a>
+            <a href="#" className={classes.icon}>
+              <BsSpotify size={18} />
+            </a>
+            <a href="#" className={classes.icon}>
+              <BsApple size={18} />
             </a>
           </div>
         </div>
